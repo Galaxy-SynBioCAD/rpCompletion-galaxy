6feeecb4fb25cb2819ec67334abba19a835602ad
joan <joan.herisson@univ-evry.fr>
2020-05-05 16:20:00 +0200

    fixes

--- run.py
+++ run.py
@@ -8,8 +8,28 @@ Created on May 4 2020
 """
 import sys
 sys.path.insert(0, '/home/rpReader')
+from rpReader import build_parser as rpReader_buildparser
 from rpReader import entrypoint as rpReader_entrypoint
+from tarfile import open as tarfile_open
+from tempfile import TemporaryDirectory as tempfile_tempdir
+from os import path as os_path
+
 
 
 if __name__ == "__main__":
-    rpReader_entrypoint(sys.argv[1:]+['-sm', 'db'])
+    parser = rpReader_buildparser()
+    params = parser.parse_args()
+
+    print(params)
+
+    with tempfile_tempdir() as tmpdirname:
+        args = [
+            '-rp2paths_compounds', params.rp2paths_compounds,
+            '-rp2_pathways', params.rp2_pathways,
+            '-rp2paths_pathways', params.rp2paths_pathways,
+            '-output', tmpdirname,
+            '-sm', 'db'
+            ]
+        rpReader_entrypoint(args)
+        with tarfile_open(fileobj=params.output, mode='w:xz') as tf:
+            tf.add(tmpdirname, arcname=os_path.basename(params.output))
