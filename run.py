b54285bda9db281eb15a276f4b81eee8fb934e82
joan <joan.herisson@univ-evry.fr>
2020-06-03 10:11:54 +0200

    comment

--- run.py
+++ run.py
@@ -18,10 +18,15 @@ from os import listdir as os_listdir
 
 
 if __name__ == "__main__":
+
+    # Parse arguments with the tool parser
     parser = tool_buildparser()
     params = parser.parse_args()
 
+    # Process in a temporary folder that will be automatically removed after exit
     with tempfile_tempdir() as tmpdirname:
+
+        # Prepare arguments for the tool
         args = [
             '-rp2paths_compounds', params.rp2paths_compounds,
             '-rp2_pathways', params.rp2_pathways,
@@ -29,7 +34,11 @@ if __name__ == "__main__":
             '-output', tmpdirname,
             '-sm', 'db'
             ]
+
+        # Run the tool
         tool_entrypoint(args)
+
+        # Format ouput data as expected by the wrapper
         with tarfile_open(params.output, mode='w:gz') as tf:
             for name in os_listdir(tmpdirname):
                 tf.add(tmpdirname+"/"+name, arcname=name)
