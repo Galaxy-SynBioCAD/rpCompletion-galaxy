2883d3938e34f0cea608919c9b4102cb8c543bf1
joan <joan.herisson@univ-evry.fr>
2020-06-08 11:39:58 +0200

    archive name

--- run.py
+++ run.py
@@ -41,4 +41,4 @@ if __name__ == "__main__":
         # Format ouput data as expected by the wrapper
         with tarfile_open(params.output, mode='w:gz') as tf:
             for name in os_listdir(tmpdirname):
-                tf.add(tmpdirname+"/"+name)
+                tf.add(tmpdirname+"/"+name, arcname=name)
